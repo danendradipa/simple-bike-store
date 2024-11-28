3013d58b9dbf7e27f0e665bda64e2f571bfdfd3b
danendradipa <danendrach@gmail.com>
2024-11-28 16:13:55 +0700

    add delete bike

---
+++ admin/delete_bike.php
@@ -0,0 +1,16 @@
+<?php
+include '../db.php';
+include '../config.php';
+
+if (!isLoggedIn() || !isAdmin()) {
+    header("Location: ../login.php");
+    exit();
+}
+
+$id = $_GET['id'];
+$stmt = $pdo->prepare("DELETE FROM bikes WHERE id = ?");
+$stmt->execute([$id]);
+
+header("Location: data_sepeda.php");
+exit();
+?>
